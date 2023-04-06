4d1fea867f0243e9df91ed212aa169ca09119d36
zeze33629 <zeze33629@gmail.com>
2023-04-06 13:31:44 -0400

    Cinema done
    Cinema should be done

--- Code/Cinema.java
+++ Code/Cinema.java
@@ -7,21 +7,20 @@ public class Cinema {
     static final String cinemaName = "ThaBest Cinema inc";
     final int branchNumber;
     protected int numberOfRoom;
-    // protected ArrayList<Staff> staff_List;
+    protected ArrayList<Staff> staff_List;
     protected ArrayList<Movie> movie_List;
 
-
-    // public Cinema() {
-    // }
+    public Cinema() {
+    }
 
     public Cinema(int branchNumber) {
         this.branchNumber = branchNumber;
     }
 
-    public Cinema(int branchNumber, int numberOfRoom, /*ArrayList<Staff> staff_List,*/ ArrayList<Movie> movie_List) {
+    public Cinema(int branchNumber, int numberOfRoom, ArrayList<Staff> staff_List, ArrayList<Movie> movie_List) {
         this.branchNumber = branchNumber;
         this.numberOfRoom = numberOfRoom;
-        // this.staff_List = staff_List;
+        this.staff_List = staff_List;
         this.movie_List = movie_List;
     }
     
@@ -34,15 +33,41 @@ public class Cinema {
         return s_List;
     }
 
-    public static void main(String[] args) {
-        Movie m1 = new Movie("Hello w", 2, 2023, 4, 6, 12, 30);
-        Movie m2 = new Movie("allo z", 1, 2023, 4, 6, 13, 30);
-        Movie m3 = new Movie("ello p", 3, 2023, 4, 6, 14, 30);
-        Movie m4 = new Movie("zllo m", 5, 2023, 4, 6, 15, 30);
-        ArrayList<Movie> m = new ArrayList<>(); 
-        m.add(m1); m.add(m2); m.add(m3); m.add(m4);
-        Cinema c = new Cinema(1, 4, m);
-        
-        System.out.println(c.showMovies());
+    public void addMovie(Movie movie) {
+        movie_List.add(movie);
     }
+
+    public void addStaff(Staff staff) {
+        staff_List.add(staff);
+    }
+
+    public int getBranchNumber() {
+        return this.branchNumber;
+    }
+
+
+    public int getNumberOfRoom() {
+        return this.numberOfRoom;
+    }
+
+    public void setNumberOfRoom(int numberOfRoom) {
+        this.numberOfRoom = numberOfRoom;
+    }
+
+    public ArrayList<Staff> getStaff_List() {
+        return this.staff_List;
+    }
+
+    public void setStaff_List(ArrayList<Staff> staff_List) {
+        this.staff_List = staff_List;
+    }
+
+    public ArrayList<Movie> getMovie_List() {
+        return this.movie_List;
+    }
+
+    public void setMovie_List(ArrayList<Movie> movie_List) {
+        this.movie_List = movie_List;
+    }
+
 }
