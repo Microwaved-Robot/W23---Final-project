7af969b548beb42f0bfcab491f9ec1679a31438a
zeze33629 <zeze33629@gmail.com>
2023-04-06 09:51:55 -0400

    cinama class var

--- Code/Cinema.java
+++ Code/Cinema.java
@@ -1,5 +1,13 @@
 package Code;
 
+import java.util.ArrayList;
+
 public class Cinema {
+    static final String cinemaName;
+    final int branchNumber;
+    protected int numberOfRoom;
+    protected ArrayList<Staff> staff_List;
+    protected ArrayList<Movie> movie_List;
+
     
 }
