293c2c8c8775a7376dca4da9ba1d8125ee98d6d5
zeze33629 <zeze33629@gmail.com>
2023-04-06 10:07:21 -0400

    Update Movie.java
    actual :100:

--- Code/Movie.java
+++ Code/Movie.java
@@ -3,7 +3,52 @@ package Code;
 import java.util.Date;
 
 public class Movie {
-    String name;
-    int length;
-    Date time;
+    private String name;
+    private int length;
+    private Date time;
+
+    public Movie() {
+    }
+
+    public Movie(String name, int length, Date time) {
+        this.name = name;
+        this.length = length;
+        this.time = time;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Date getTime() {
+        return this.time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " is playing for " + getLength() 
+            + " minutes at " + getTime() + " o'clock";
+    }
+
+    public boolean equals(Movie m) {
+        return m.getName().equals(this.getName()) 
+            && m.length == this.length 
+            && m.time.equals(this.time);
+    }
 }
